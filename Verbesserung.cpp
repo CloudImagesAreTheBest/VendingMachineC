8854154554d365c4002051b16fd49670458da95d
Puckbeisser <36338640+Puckbeisser@users.noreply.github.com>
2018-02-27 11:57:38 +0100

    Add files via upload

---
+++ Verbesserung.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+double Restgeld(double);
+
+
+int main()
+    {
+
+        char Pruef[]="";
+
+        cin>>setw(8)>>Pruef;
+
+        cout<<Pruef;
+
+
+
+
+
+        //Restgeld();
+    }
+
+
+
+double Restgeld(double)
+
+int Geldforderung=1;
+if(Uebergabewert_Muenzeinwurf>Geldforderung)
+    {
+        Restgeld=Uebergabewert_Muenzeinwurf-Geldforderung;
+    }
+
+return Restgeld;
+
+
+
+
+
+
+
