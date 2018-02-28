a555b4cd8db12d05ecb3384691f3857e7a00fe5d
CloudImagesAreTheBest <36044424+CloudImagesAreTheBest@users.noreply.github.com>
2018-02-28 08:02:09 +0100

    Update Verbesserung.cpp

--- Verbesserung.cpp
+++ Verbesserung.cpp
@@ -3,7 +3,7 @@
 #include <iomanip>
 
 using namespace std;
-double Restgeld(double);
+double Restgeld(double, double);
 
 
 int main()
@@ -12,7 +12,9 @@ int main()
         char Pruef[]="";
 
         cin>>setw(8)>>Pruef;
-
+        // GLPI >>>
+        CharPointerToInt(char*)
+        // GLPI <<<
         cout<<Pruef;
 
 
@@ -24,15 +26,15 @@ int main()
 
 
 
-double Restgeld(double)
-
-int Geldforderung=1;
-if(Uebergabewert_Muenzeinwurf>Geldforderung)
-    {
-        Restgeld=Uebergabewert_Muenzeinwurf-Geldforderung;
-    }
-
-return Restgeld;
+double Restgeld(double Uebergabewert_Muenzeinwurf,double Geldforderung)
+{
+    double Restgeld = 0.0;
+    if(Uebergabewert_Muenzeinwurf>Geldforderung)
+        {
+            Restgeld=Uebergabewert_Muenzeinwurf-Geldforderung;
+        }
+    return Restgeld;
+}
 
 
 
