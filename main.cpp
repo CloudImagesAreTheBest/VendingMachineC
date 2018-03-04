006c21d7ebf5a8e20b00d9b8c063502bbd8e3b53
GlicPi <34307263+GlicPi@users.noreply.github.com>
2018-03-04 13:27:33 +0100

    FINAL
    FINAL

--- main.cpp
+++ main.cpp
@@ -1,69 +1,61 @@
-
+#include <cstdlib>
 #include <iostream>
 #include <iomanip>
 #include <math.h>
 #include <conio.h>
-// KeMay >>>
 #include <stdio.h>
 #include<stdlib.h>
-// KeMay <<<
 #include<string>
+
 
 using namespace std;
 
 // Funktion Deklaration
 int Pruefziffer();
 void Benutzeroberflaeche();
-int Auswahl();
+double Auswahl();
 float fEingabe(string,int);
 int iEingabe(string,int);
 int CharPointerToInt(char);
+void CoinOutput(double);
+int CalcRestCoin(int,int &);
+double dGeldEinwurf(double);
+void OutputItem(string);
+void vCloseApplication();
+
 
 int main()
 {
-
-	Pruefziffer();
-	Benutzeroberflaeche();
-	Auswahl();
+	double dPuffer=0.0;
+	
+	while(true) 
+	{// always true because a real vending machine would never stop it´s program.
+		Pruefziffer();
+		Benutzeroberflaeche();
+		dPuffer = Auswahl();
+		dPuffer = dGeldEinwurf(dPuffer);
+		CoinOutput(dPuffer);
+		OutputItem("X");
+		vCloseApplication();
+	}
 
 
 	return 0;
 }
 
 
-
-//  Pruefziffer() muesste noch mit einer Bedingung versehen werden - wenn die errechnete Pruefziffer nicht mit der eingegebenen uebereinstimmt muesste das Programm
-//  abbrechen, Ist sehr banal codiert -> ohne Array
-
-//  Auswahl() ist noch fehlerhaft von der Logik - ich wusste nicht genau, was wir machen wollen, wenn wir etwas anderes eingeben, als vorgegebene cases
-
-
-
-/*
-//  Ungefährer Aufbau ??
-Pruefziffer();
-Benutzeroberflaeche();
-Auswahl();
-Muenzeinwurf();
-//Frage ob eigener Tasse oder Plastikbecher -> Aufpreis
-//Frage ob noch ein Becher gewünscht ist oder ob Restgeld ausgegeben werden soll
-//ggf neue Auswahl über die Oberfläche und Auswahl
-Oberfläche();
-Restgeldausgabe();
-Getränkeausgabe();
-*/
-
-
-// Erster Versuch Geldeinwurf - Muesste aber mit Do-While mehr Sinn machen -> so wie jetzt funktionierts noch nicht bis jetzt noch keine weitere Zeit gehabt
-
-
 double dGeldEinwurf(double Geldforderung)
-{// Fertig
-	int Kategorie = 0;
+{
+	int iInput = 0;
 	double Muenzwert = 0.0;
-	while (Geldforderung <= Muenzwert)
+	cout << "------- SIMULATION------" << endl;
+	cout << "1 = 1 Cent"<<endl<<"2 = 2 Cent"<<endl<<"3 = 5 Cent"<<endl<<"4 = 10 Cent"<<endl<<"5 = 20 Cent"<<endl<<"6 = 50 Cent"<<endl<<"7 = 1 Euro"<<endl<<"8 = 2 Euro"<<endl;
+	while (Geldforderung >= Muenzwert)
 	{
-		switch (Kategorie)
+		
+		fflush(stdin);
+		cin >> iInput;
+		switch (iInput)
 		{
 		case 1:
 			Muenzwert = Muenzwert + 0.01;
@@ -96,6 +88,8 @@ double dGeldEinwurf(double Geldforderung)
 		}
 
 	}
+	cout <<endl<< "Sie haben " << Muenzwert << " eingeworfen."<<endl;
+	Muenzwert = Muenzwert - Geldforderung;
 	return Muenzwert;
 }
 
@@ -103,30 +97,19 @@ double dGeldEinwurf(double Geldforderung)
 
 
 int Pruefziffer()
-{//Todo -> Eingabe begrenzen
+{
 	int Erste = 0, Zweite = 0, Dritte = 0, Vierte = 0, Fuenfte = 0, Sechste = 0, Pruef = 0, Ergebnis_1 = 0, Ergebnis_2 = 0, Ergebnis_3 = 0,
 		Ergebnis_4 = 0, Ergebnis_5 = 0, Ergebnis_6 = 0, ErgebnisSumme = 0, Modulo = 0, Pruefzahl = 0;
     int iWeiter = 0;
 
 	do
-	{// String -> ABCD  -> |A|B|C|D|\0|
+	{
      
 		cout << "Bitte geben Sie Ihre 7-stellige Kundennummer ein" << endl;
 
 		char cKundennummer[7] = "";
-//		cin >> setw(7) >> cKundennummer;
-		// TEST
-		int i=1;
-		while (cKundennummer[i-1]!=EOF)
-		{
-			cKundennummer[i] = getchar();
-			i++;
-		}
-		// GLPI >>>
-		CharPointerToInt(cKundennummer[0]);
-		// GLPI <<<
-		cout << Pruef;
-
+		fflush(stdin);
+		cin >> cKundennummer;
 		
         Erste = CharPointerToInt(cKundennummer[0]);
         Zweite = CharPointerToInt(cKundennummer[1]);
@@ -146,7 +129,7 @@ int Pruefziffer()
         ErgebnisSumme = Ergebnis_1 + Ergebnis_2 + Ergebnis_3 + Ergebnis_4 + Ergebnis_5 + Ergebnis_6;
         Modulo = ErgebnisSumme % 10;
         Pruefzahl = 10 - Modulo;
-        // PIGLIC >>>
+        
         cout << Pruefzahl;
         if ((10 - Pruef) == Pruefzahl)
          {
@@ -156,12 +139,12 @@ int Pruefziffer()
             cout <<endl<< "Falsche Eingabe bitte erneut eingeben" << endl;
             iWeiter = 1;
          }
-        // PIGLIC <<<
+
         }while(iWeiter == 1);
 	return Pruefzahl;
 }
 void Benutzeroberflaeche()
-{//Fertig
+{
 
 	cout << endl << "#############################################################################" << endl
 		<< "# ------------------------------------------------------------------------- #" << endl
@@ -190,14 +173,12 @@ void Benutzeroberflaeche()
 
 }
 
-int Auswahl()
+double Auswahl()
 {
 
-	int Kategorie1 = 0, Geldforderung = 0;
-// PiGlic >>>
-// char Katergorie2 = 0;
+	int Kategorie1 = 0;
+	double Geldforderung = 0.0;
 	char Kategorie2 = '0';
-// PiGlic <<<
 	cin >> Kategorie1;
 
 	switch (Kategorie1)
@@ -218,7 +199,7 @@ int Auswahl()
 			cout << "Schwarzer Kaffee = 70 Cent" << endl
 				<< "Bitte werfen Sie die Muenzen in den Muenzeinwurf" << endl;
 
-			Geldforderung = Geldforderung + 70;
+			Geldforderung = Geldforderung + 0.70;
 		} break;
 
 		case 'z':
@@ -226,14 +207,14 @@ int Auswahl()
 			cout << "Kaffee mit Zucker = 80 Cent" << endl
 				<< "Bitte werfen Sie die Muenzen in den Muenzeinwurf" << endl;
 
-			Geldforderung = Geldforderung + 80;
+			Geldforderung = Geldforderung + 0.80;
 		} break;
 
 		case 'm':
 		{
 			cout << "Kaffee mit Milch = 80 Cent" << endl
 				<< "Bitte werfen Sie die Muenzen in den Muenzeinwurf";
-			Geldforderung = Geldforderung + 80;
+			Geldforderung = Geldforderung + 0.80;
 		} break;
 
 		case 'b':
@@ -241,7 +222,7 @@ int Auswahl()
 			cout << "Kaffee mit Zucker und Milch = 90 Cent" << endl
 				<< "Bitte werfen Sie die Muenzen in den Muenzeinwurf";
 
-			Geldforderung = Geldforderung + 90;
+			Geldforderung = Geldforderung + 0.90;
 		} break;
 
 		default: cout << "\"Zurueck zum Hauptmenue\"" << endl;
@@ -254,7 +235,7 @@ int Auswahl()
 		cout << "[2] Cappuchino kostet 75 Cent" << endl
 			<< "Bitte werfen Sie die Muenzen in den Muenzeinwurf" << endl;
 
-		Geldforderung = Geldforderung + 75;
+		Geldforderung = Geldforderung + 0.75;
 	} break;
 
 	case 3:
@@ -262,7 +243,7 @@ int Auswahl()
 		cout << "[3] Latte Macchiato kostet 85 Cent" << endl
 			<< "Bitte werfen Sie die Muenzen in den Muenzeinwurf" << endl;
 
-		Geldforderung = Geldforderung + 85;
+		Geldforderung = Geldforderung + 0.85;
 	} break;
 
 	case 4:
@@ -270,7 +251,7 @@ int Auswahl()
 		cout << "[4] schwarzer Tee kostet 70 Cent" << endl
 			<< "Bitte werfen Sie die Muenzen in den Muenzeinwurf" << endl;
 
-		Geldforderung = Geldforderung + 70;
+		Geldforderung = Geldforderung + 0.70;
 	} break;
 
 	case 5:
@@ -278,7 +259,7 @@ int Auswahl()
 		cout << "[5] Fruechtetee kostet 70 Cent" << endl
 			<< "Bitte werfen Sie die Muenzen in den Muenzeinwurf" << endl;
 
-		Geldforderung = Geldforderung + 70;
+		Geldforderung = Geldforderung + 0.70;
 	} break;
 
 	case 6:
@@ -286,7 +267,7 @@ int Auswahl()
 		cout << "[6] Gemuesebruehe kostet 95 Cent" << endl
 			<< "Bitte werfen Sie die Muenzen in den Muenzeinwurf" << endl;
 
-		Geldforderung = Geldforderung + 95;
+		Geldforderung = Geldforderung + 0.95;
 	} break;
 
 	default: cout << "Sie haben eine falsche Taste gedrueckt";
@@ -362,3 +343,93 @@ int CharPointerToInt(char cZeichen)
 			break;
 	}
 }
+
+
+
+/*
+Function name: OutputItem
+Arguments: sItemName - this is the item which is chosen by the user
+Output / Result: Tell the user that he got the item and say bye
+*/
+void OutputItem(string sItemName)
+{
+	if (sItemName!="X")
+	{
+		cout << "Hier haben Sie Ihren '" << sItemName << "'." << endl <<
+				"Vielen Dank fuer Ihren Einkauf!" << endl;
+	} else {
+		cout << endl << "Vielen Dank fuer Ihren Einkauf!" << endl;
+	}
+}
+
+/*
+Function name: CalcRestCoin
+Arguments:
+- iCoinValue: The value of the coin in cent - so 2 euros = 200 cents, this argument will be committed through
+the CoinOutput function
+- &iCurrentMoney - This is the current rest money which will be automatically subtracted by the corresponding
+coin value
+Output / Result: Displays to the console screen the current rest money in coins (euro currency)
+*/
+int CalcRestCoin(int iCoinValue, int &iCurrentMoney)
+{
+	int iReturn = iCurrentMoney / iCoinValue;
+	iCurrentMoney = iCurrentMoney - iReturn * iCoinValue;
+	return iReturn;
+}
+
+/*
+Function name: CoinOutput
+Arguments: dChange; This is the rest money which will be calculated through the input functionality
+Output / Result: Displays to the console screen the current rest money in coins (euro currency)
+*/
+void CoinOutput(double dChange)
+{
+	int iCurrentMoney = dChange * 100;
+	int iMoneyArr[] = { 200, 100, 50, 20, 10, 5, 2, 1 };
+	int iCoins[sizeof(iMoneyArr) / sizeof(int)];
+
+	char* cCoinCaptions[] = {
+		"2 Eurostueck",
+		"1 Eurostueck",
+		"50 Centstueck",
+		"20 Centstueck",
+		"10 Centstueck",
+		"5 Centstueck",
+		"2 Centstueck",
+		"1 Centstueck"
+	};
+	
+	cout << endl << "Ihr Wechselgeld betraegt:" << endl;
+	
+	int f = 0;
+	for (int i : iMoneyArr)
+	{
+		iCoins[f] = CalcRestCoin(i, iCurrentMoney);
+		if (iCoins[f] != 0) {
+			cout << iCoins[f] << " x " << cCoinCaptions[f] << endl;
+		}
+		f++;
+	}
+}
+
+void vCloseApplication()
+{
+	int iStatus = 0;
+	
+	cout << endl << "----Simulation----"<< endl;
+	cout << "Moechten sie das Program beenden ?"<<endl;
+	cout << "0 = Ende | 1 = Weiter" << endl;
+	cin >> iStatus;
+	
+	if(iStatus == 1)
+	{
+		system("cls");
+	} else 
+	{
+		exit(1);
+	}
+	
+}
+
+
